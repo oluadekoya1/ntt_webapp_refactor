5d4f66652cd67685cb420add67f59ff916d9a853
tester_adekoya <tester_adekoya>
2017-04-26 16:46:33 +0100

    Upload Page UI

--- src/features/uploadPage/uploadPage.controller.js
+++ src/features/uploadPage/uploadPage.controller.js
@@ -9,11 +9,79 @@ export default function UploadPageController($scope ,$state, $stateParams, appSe
     //$scope.myApps = function(){
     //    $state.go('review-app', {username :$scope.user });
     //};
-
-
-
-
     $scope.user = appServices.getUserName();
+
+
+    //GET THE FILE INFORMATION
+
+    $scope.setFiles = function(element) {
+
+
+        var index =angular.element(element).scope().$index;
+        var Dfile = element.files[0].name;
+        $scope.retrievedFileName= Dfile;
+
+
+        //$scope.files =[];
+        //$scope.$apply(function (){
+        //
+        //    //STORE THE FILE OBJECT ARRAY
+        //    for (var i=0; i<element.files.length; i++){
+        //        $scope.files.push(element.files[i]);
+        //        $scope.this.fileName[i]=element.files[i].name;
+        //    }
+        //});
+
+
+
+    };
+
+
+    ////START
+    //
+    //// NOW UPLOAD FILES
+    //
+    //$scope.uploadFile =function(){
+    //
+    //
+    //    //FILL FormData WITH FILE DETAILS
+    //
+    //    var data = new FormData();
+    //
+    //    for (var i in $scope.files){
+    //        data.append("uploadedFile", $scope.files[i]);
+    //    }
+    //    console.log(files[i]);
+    //
+    //    //ADD LISTENRS
+    //    var objXhr = new XMLHttpRequest();
+    //    objXhr.addEventListener("progress", updateProgress, false);
+    //    objXhr.addEventListener("load", transferComplete, false);
+    //
+    //    //SEND FILE DETAILS TO THE API
+    //    objXhr.open("POST", "/api/fileupload/")
+    //    objXhr.send(data)
+    //}
+    //
+    ////UPDATE PROGRESS BAR
+    //
+    //function updateProgress(element) {
+    //    if(element.lengthComputable){
+    //        document.getElementById('pro').setAttribute('value', element.loaded);
+    //        document.getElementById('pro').setAttribute('max', element.total);
+    //    }
+    //}
+    //
+    //function transferComplete(element){
+    //    alert("Files uploaded successfully")
+    //}
+    //
+    //
+    //
+    ////END
+
+
+
 
 }
 
