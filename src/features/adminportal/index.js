144dd18ff1a98a61f8a71075bb37dba42e6a8ac2
tester_adekoya <tester_adekoya>
2017-05-16 18:11:01 +0100

    add comment

--- src/features/adminportal/index.js
+++ src/features/adminportal/index.js
@@ -9,6 +9,8 @@ import mainMenu from '../../directives/js/main-menu';
 import menubar from '../../directives/js/menubar';
 import appServices from '../../factories/appServices';
 
+//add some comments
+
 export default angular.module('app.adminPortal', [uirouter, menubar, mainMenu, appServices])
     .config(routing)
     .controller('adminPortalController', adminPortalController)
