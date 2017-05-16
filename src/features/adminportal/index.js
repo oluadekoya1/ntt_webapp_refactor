fa172b1e8b915b426afe8d77e9d929260c511c0d
tester_adekoya <tester_adekoya>
2017-05-16 19:27:59 +0100

    removed unused imports from admin portal

--- src/features/adminportal/index.js
+++ src/features/adminportal/index.js
@@ -5,13 +5,9 @@ import uirouter from 'angular-ui-router';
 import './adminPortal.scss';
 import routing from './adminPortal.routes';
 import adminPortalController from './adminPortal.controller';
-import mainMenu from '../../directives/js/main-menu';
-import menubar from '../../directives/js/menubar';
 import appServices from '../../factories/appServices';
 
-//add some comments
-
-export default angular.module('app.adminPortal', [uirouter, menubar, mainMenu, appServices])
+export default angular.module('app.adminPortal', [uirouter, appServices])
     .config(routing)
     .controller('adminPortalController', adminPortalController)
     .name;
