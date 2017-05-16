6f6808bfc929ba8e4e3faa1744f9566349f7cb1d
tester_adekoya <tester_adekoya>
2017-05-16 19:50:31 +0100

    added admin portal

--- src/features/admin_portal/admin_portal.routes.js
+++ src/features/admin_portal/admin_portal.routes.js
@@ -7,9 +7,9 @@ export default function routes($stateProvider) {
     $stateProvider
         .state('admin', {
             url: '/admin',
-            template: require('./admin.html'),
-            controller: 'adminController',
-            controllerAs: 'admin'
+            template: require('./admin_portal.html'),
+            controller: 'adminPortalController',
+            controllerAs: 'adminPortal'
         });
 }
 
